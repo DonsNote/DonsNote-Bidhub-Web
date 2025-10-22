50479ab335dd6c8c59d5b905ddbcea76237e0945
DonsNote <neveradio@gmail.com>
2025-10-23 07:00:35 +0900

    fix_supa

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  // 개발 환경에서 외부 IP 접근 허용
+  allowedDevOrigins: [
+    'http://52.184.83.107:3000',
+    'http://52.184.83.107',
+  ],
 };
 
 export default nextConfig;
